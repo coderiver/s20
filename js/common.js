4c30452d61c657ce037f939c36ae7bf370b6e568
moroz <arturmorozv@gmail.com>
2015-05-13 18:16:12 +0300

    landing and one room

--- js/common.js
+++ js/common.js
@@ -1,19 +1,110 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// nav
+	(function () {
+		$('.js-nav-btn').on('click', function () {
+			$('.js-nav').slideToggle();
+			return false;
+		});
+	}());
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	// share
+	(function () {
+		$('.js-share-btn').on('click', function () {
+			$('.js-share').slideToggle();
+			return false;
+		});
+	}());
 
-	console.log($('body').html());
+	// benefits
+	(function () {
+		var link = $('.js-benefits-link'),
+			el = $('.js-benefits-el'),
+			popup = $('.js-benefits-popup'),
+			close = $('.js-benefits-close');
+		link.on('click', function () {
+			var index = $(this).index();
+			el.hide();
+			el.eq(index).show();
+			popup.fadeIn();
+			return false;
+		});
+		close.on('click', function () {
+			popup.fadeOut();
+		})
+	}());
+
+	// about
+	(function () {
+		$('.js-about-link').on('click', function () {
+			$('.js-about-more').slideToggle();
+			return false;
+		});
+	}());
+
+	// fancybox
+	(function () {
+		var fb = $('.fancybox'),
+			fbForm = $('.fancybox-form');
+		if (fb.length) {
+			fb.fancybox({
+				margin: 10,
+				padding: 0,
+				prevEffect: 'none',
+				nextEffect: 'none',
+				helpers: {
+					overlay: {
+				    	locked: false
+					},
+					title : {
+						type : 'inside' // 'float', 'inside', 'outside' or 'over'
+					}
+				}
+			});
+		};
+		if (fbForm.length) {
+			fbForm.fancybox({
+				margin: 10,
+				padding: 0,
+				helpers: {
+					overlay: {
+				    	locked: false
+					}
+				}
+			})
+		};
+	}());
+
+	// nav
+	(function () {
+
+	}());
+
+	// progress
+	(function () {
+		var sl = $('.js-sl');
+		if (sl.length) {
+			sl.slick({
+				infinite: true,
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				slide: '.js-sl-item',
+				responsive: [{
+				    breakpoint: 1020,
+				    settings: {
+				    	slidesToShow: 2,
+				    	slidesToScroll: 2
+				    }
+				},
+				{
+				    breakpoint: 760,
+				    settings: {
+				    	slidesToShow: 1,
+				    	slidesToScroll: 1
+				    }
+				}]
+			});
+		};
+	}());
+
 });
